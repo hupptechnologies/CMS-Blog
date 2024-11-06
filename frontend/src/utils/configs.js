a07579076474ea15a3719b46efeb9023ed731a29
hupptechnologies <git@hupp.in>
2024-11-06 14:33:49 +0530

    Remove extra env variables

--- frontend/src/utils/configs.js
+++ frontend/src/utils/configs.js
@@ -4,7 +4,6 @@ export const ConfigCloudnary = {
   cloudName: process.env.REACT_APP_CLOUDNAME,
   apiKey: process.env.REACT_APP_API_KEY,
   apiSecret: process.env.REACT_APP_API_SECRET,
-  envVariable: `CLOUDINARY_URL=cloudinary://${process.env.REACT_APP_API_KEY}:${process.env.REACT_APP_API_SECRET}@${process.env.REACT_APP_CLOUDNAME}`
 };
 
 export const API_URL = process.env.REACT_APP_API_URL;
@@ -15,24 +14,8 @@ export const dummyUser = {
 };
 
 export const googleAnalyticConfigs = {
-  STREAM_NAME: process.env.REACT_APP_STREAM_NAME,
-  STREAM_URL: process.env.REACT_APP_STREAM_URL,
-  STREAM_ID: process.env.REACT_APP_STREAM_ID,
   MEASUREMENT_ID: process.env.REACT_APP_MEASUREMENT_ID
 };
 
 export const getFlagURl = (countryCode) =>
   `https://flagsapi.com/${countryCode.toUpperCase()}/shiny/64.png`;
-
-
-// # Cloudinary Credentials
-// REACT_APP_CLOUDNAME=dx12uvyye
-// REACT_APP_PRESET=Test_preset
-// REACT_APP_API_KEY=344972316838632
-// REACT_APP_API_SECRET=W3EbS2M2MtEUtH7Dng6PEtDYlRk
-
-// # Google Analytics Config
-// REACT_APP_STREAM_NAME=cms
-// REACT_APP_STREAM_URL=https://192.168.1.64:3000
-// REACT_APP_STREAM_ID=8531453273
-// REACT_APP_MEASUREMENT_ID=G-3GV4XX6C8V
